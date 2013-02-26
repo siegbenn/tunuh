cd4c351b06094d355b17b68b35ad538edf58939b
Bennett Siegel <bennett.e.siegel@gmail.com>
2013-02-25 19:09:18 -0500

    Added raised tunings.

--- brain.php
+++ brain.php
@@ -380,6 +380,96 @@
 				$title = 'Drop D1';
 				$desc = '';
 				break;
+                
+                
+                
+            case 'raise-f':
+                $notes = array(
+					array('F', 'https://s3.amazonaws.com/tuning-website/notes/0-f.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-f.mp3'),
+					array('A#', 'https://s3.amazonaws.com/tuning-website/notes/1-asharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-asharp.mp3'),
+					array('D#', 'https://s3.amazonaws.com/tuning-website/notes/1-dsharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-dsharp.mp3'),
+					array('G#', 'https://s3.amazonaws.com/tuning-website/notes/1-gsharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-gsharp.mp3'),
+					array('C', 'https://s3.amazonaws.com/tuning-website/notes/2-c.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-c.mp3'),
+					array('F', 'https://s3.amazonaws.com/tuning-website/notes/2-f.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-f.mp3')
+				);
+				$title = 'Raised F';
+				$desc = '';
+				break;
+                
+                
+                
+            case 'raise-gb':
+                $notes = array(
+    				array('F#', 'https://s3.amazonaws.com/tuning-website/notes/0-fsharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-fsharp.mp3'),
+					array('B', 'https://s3.amazonaws.com/tuning-website/notes/1-b.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-b.mp3'),
+					array('E', 'https://s3.amazonaws.com/tuning-website/notes/1-e.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-e.mp3'),
+					array('A', 'https://s3.amazonaws.com/tuning-website/notes/2-a.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-a.mp3'),
+					array('C#', 'https://s3.amazonaws.com/tuning-website/notes/2-csharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-csharp.mp3'),
+					array('F#', 'https://s3.amazonaws.com/tuning-website/notes/2-fsharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-fsharp.mp3')
+				);
+				$title = 'Raised F#/Gb';
+				$desc = '';
+				break;
+                
+                
+                
+            case 'raise-g':
+                $notes = array(
+        			array('G', 'https://s3.amazonaws.com/tuning-website/notes/0-g.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-g.mp3'),
+					array('C', 'https://s3.amazonaws.com/tuning-website/notes/1-c.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-c.mp3'),
+					array('F', 'https://s3.amazonaws.com/tuning-website/notes/1-f.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-f.mp3'),
+					array('A#', 'https://s3.amazonaws.com/tuning-website/notes/2-asharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-asharp.mp3'),
+					array('D', 'https://s3.amazonaws.com/tuning-website/notes/2-d.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-d.mp3'),
+					array('G', 'https://s3.amazonaws.com/tuning-website/notes/2-g.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-g.mp3')
+				);
+				$title = 'Raised G (Terz)';
+				$desc = '';
+				break;
+                
+                
+                
+            case 'raise-ab':
+                $notes = array(
+            		array('G#', 'https://s3.amazonaws.com/tuning-website/notes/0-gsharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-gsharp.mp3'),
+					array('C#', 'https://s3.amazonaws.com/tuning-website/notes/1-csharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-csharp.mp3'),
+					array('F#', 'https://s3.amazonaws.com/tuning-website/notes/1-fsharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-fsharp.mp3'),
+					array('B', 'https://s3.amazonaws.com/tuning-website/notes/2-b.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-b.mp3'),
+					array('D#', 'https://s3.amazonaws.com/tuning-website/notes/2-dsharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-dsharp.mp3'),
+					array('G#', 'https://s3.amazonaws.com/tuning-website/notes/2-gsharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-gsharp.mp3')
+				);
+				$title = 'Raised G#/Ab';
+				$desc = '';
+				break;
+                
+                
+                
+            case 'raise-a':
+                $notes = array(
+                	array('A', 'https://s3.amazonaws.com/tuning-website/notes/0-a.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-a.mp3'),
+					array('D', 'https://s3.amazonaws.com/tuning-website/notes/0-d.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-d.mp3'),
+					array('G', 'https://s3.amazonaws.com/tuning-website/notes/0-g.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-g.mp3'),
+					array('C', 'https://s3.amazonaws.com/tuning-website/notes/1-c.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-c.mp3'),
+					array('E', 'https://s3.amazonaws.com/tuning-website/notes/1-e.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-e.mp3'),
+					array('A', 'https://s3.amazonaws.com/tuning-website/notes/2-a.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-a.mp3')
+				);
+				$title = 'Raised A';
+				$desc = '';
+				break; 
+                
+                
+                
+            case 'raise-bb':
+                $notes = array(
+                    array('A#', 'https://s3.amazonaws.com/tuning-website/notes/0-asharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-asharp.mp3'),
+					array('D#', 'https://s3.amazonaws.com/tuning-website/notes/0-dsharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-dsharp.mp3'),
+					array('G#', 'https://s3.amazonaws.com/tuning-website/notes/0-gsharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-gsharp.mp3'),
+					array('C#', 'https://s3.amazonaws.com/tuning-website/notes/1-csharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-csharp.mp3'),
+					array('F', 'https://s3.amazonaws.com/tuning-website/notes/1-f.ogg', 'http://s3.amazonaws.com/tuning-website/notes/1-f.mp3'),
+					array('A#', 'https://s3.amazonaws.com/tuning-website/notes/2-asharp.ogg', 'http://s3.amazonaws.com/tuning-website/notes/2-asharp.mp3')
+				);
+				$title = 'Raised A#/Bb';
+				$desc = '';
+				break;  
 			default:
 				$notes = array(
 					array('E', 'https://s3.amazonaws.com/tuning-website/notes/0-e.ogg', 'http://s3.amazonaws.com/tuning-website/notes/0-e.mp3'),
